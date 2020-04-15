fd931d44797aaea5d9e5e9914f78176a1b1ff00d
francis DAVID <contato@fdavid.com.br>
2020-04-15 17:46:13 -0300

    Echo payload

--- publisher.sh
+++ publisher.sh
@@ -5,15 +5,16 @@ source common.sh
 TOPIC_TEST=$(psql -c "select token from things where id = 1" -t)
 SENSOR_TEST=$(psql -c "select id from thingssensors where id = 1" -t)
 
-echo "Topic: $TOPIC_TEST"
-echo "Sensor: $SENSOR_TEST"
-
 VALUE=$(((RANDOM % 100) +1))
 DATE=$(date '+%Y%m%d%H%M%S')
 
 PAYLOAD="{\"dt\": \"$DATE\", \"sensor\": $SENSOR_TEST, \"value\": $VALUE}"
 USER=fdavid
 PASSWD=12345678
+
+echo "Topic: $TOPIC_TEST"
+echo "Sensor: $SENSOR_TEST"
+echo "Payload: $PAYLOAD"
 
 mosquitto_pub -h localhost -t $TOPIC_TEST -m "$PAYLOAD" -u $USER -P $PASSWD
 
