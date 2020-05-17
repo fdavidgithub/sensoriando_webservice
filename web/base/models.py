63a8c7e6e51458466e3e2722f3f6279a66315741
francis DAVID <contato@fdavid.com.br>
2020-05-17 00:42:20 -0300

    proxy models to admin

--- web/base/models.py
+++ web/base/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 from .legacy_tables import *
 from .legacy_views import *
 
-#class Account(Accounts):
+class Account(Accounts):
 #    user = models.OneToOneField(User, on_delete=models.CASCADE)
 #
 #    def __init__(self, *args, **kwargs):
@@ -16,10 +16,29 @@ from .legacy_views import *
 #        self._meta.get_field('usetrigger').default = False
 #        self.instance._state.adding = False
 #    
-#    def __str__(self):
-#        return self.city
-#
-#    class Meta:
-#        verbose_name_plural = 'Profile'
-#        proxy = True        
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = "Account"
+        verbose_name_plural = 'Accounts'
+        proxy = True        
+
+class Thing(Things):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Thing"
+        verbose_name_plural = 'Things'
+        proxy = True        
+
+class Sensor(Sensors):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Sensor"
+        verbose_name_plural = 'Sensors'
+        proxy = True        
 
