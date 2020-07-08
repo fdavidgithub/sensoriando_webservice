341922e598f2b047daa84996cc2133271dcb46ce
francis DAVID <contato@fdavid.com.br>
2020-07-07 22:33:41 -0300

    Include model modules for admin

--- web/base/admin.py
+++ web/base/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import djAccount, djThing, djSensor
+from .models import djAccount, djThing, djSensor, djPlan, djModule
 
 # Register your models here.
 admin.site.register(djAccount)
 admin.site.register(djThing)
 admin.site.register(djSensor)
+admin.site.register(djPlan)
+admin.site.register(djModule)
 
--- web/base/models.py
+++ web/base/models.py
@@ -6,16 +6,16 @@ from django.dispatch import receiver
 from .legacy_tables import *
 from .legacy_views import *
 
+class djPlan(Plans):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Plan"
+        verbose_name_plural = 'Plans'
+        proxy = True        
+
 class djAccount(Accounts):
-#    user = models.OneToOneField(User, on_delete=models.CASCADE)
-#
-#    def __init__(self, *args, **kwargs):
-#        self._meta.get_field('dt').default = datetime.datetime.today()
-#        self._meta.get_field('status').default = True
-#        self._meta.get_field('ispublic').default = True
-#        self._meta.get_field('usetrigger').default = False
-#        self.instance._state.adding = False
-#    
     def __str__(self):
         return self.username
 
@@ -40,5 +40,14 @@ class djSensor(Sensors):
     class Meta:
         verbose_name = "Sensor"
         verbose_name_plural = 'Sensors'
-        proxy = True        
+        proxy = True   
+
+class djModule(Modules):
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Module"
+        verbose_name_plural = 'Modules'
+        proxy = True   
 
