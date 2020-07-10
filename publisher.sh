91e774c4b3e341cdacbb6ceed60f5f21845924c2
francis DAVID <contato@fdavid.com.br>
2020-07-09 23:21:14 -0300

    Create custom payload to sensor 3: message

--- publisher.sh
+++ publisher.sh
@@ -13,20 +13,24 @@ if [ -z $SENSOR ]; then
     export SENSOR=1
 fi
 
-if [ $SENSOR -eq 2 ]; then
-    echo "Message is incompatible with it"
-    exit
-fi
-
 UUID=$(psql -c "select uuid from things where id = $THING" -t)
 SENSOR=$(psql -c "select id from modulessensors where id = $SENSOR" -t)
 QOS=1		# 0, 1 or 2
 RETAINED=1 	# 1 true or 0 false
 
 VALUE=$(((RANDOM % 100) +1))
+if [ $SENSOR -eq 3 ]; then
+    VALUE="abc $VALUE"
+fi
+
 DATE=$(date -u '+%Y%m%d%H%M%S')
 
-PAYLOAD="{\"dt\": \"$DATE\", \"value\": $VALUE}"
+if [ $SENSOR -eq 3 ]; then
+    PAYLOAD="{\"dt\": \"$DATE\", \"value\": \"$VALUE\"}"
+else
+    PAYLOAD="{\"dt\": \"$DATE\", \"value\": $VALUE}"
+fi
+
 USER=fdavid
 PASSWD=12345678
 
