2157fead2070ec9d3e11edcf899bda2a9f687a1d
francis DAVID <contato@fdavid.com.br>
2022-05-07 20:52:38 -0300

    [upd] show all records, no pagination

--- core/settings.py
+++ core/settings.py
@@ -32,11 +32,13 @@ ALLOWED_HOSTS = ['*']
 LOCAL_APPS = [
         'base',
         'api',
+        'overview',
+
 ]
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+#    'PAGE_SIZE': 10
 }
 
 DEFAULT_APPS = [
