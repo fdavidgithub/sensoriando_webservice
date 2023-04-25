ddcfd91c66a729d9283a16a2e244a6b39866c30e
francis DAVID <contato@fdavid.com.br>
2023-04-25 15:47:44 -0300

    feat: name of country

--- overview/views.py
+++ overview/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from base.views import callAPI
 
+import pycountry
+
 def Filters():
     sensors = callAPI("/sensor/")
     sensortags = callAPI("/sensor/tag/")
@@ -15,9 +17,17 @@ def Filters():
     return filters
 
 def Home(request):
+    jsonResult = callAPI("/data/public/thing")
+
+    for item in jsonResult:
+        if "country" in item["account"]:
+            country_code = item["account"]["country"]
+            country_name = pycountry.countries.get(alpha_2=country_code).name
+            item["account"]["country"] = country_name
+
     contexts = {
         'filters': Filters(),
-        'things': callAPI("/data/public/thing"),
+        'things': jsonResult,
 
     }
            
