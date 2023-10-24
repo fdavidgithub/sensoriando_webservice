8f55414e55cac9139575f7612fd8b620ebffb79b
francis DAVID <contato@fdavid.com.br>
2023-10-24 20:18:29 -0300

    feat: Layout like workpress theme

--- core/settings.py
+++ core/settings.py
@@ -147,12 +147,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
-
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-
-# Setting to specify this route
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+                    os.path.join(BASE_DIR, 'static'),
+                   )
+
+STATIC_ROOT = os.path.join('staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
+
