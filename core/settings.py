4ba74c6cad05cbd328f0990134b4dea9649118ca
francis DAVID <contato@fdavid.com.br>
2024-07-08 20:31:06 -0300

    issue #53 closed

--- core/settings.py
+++ core/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PREFIX_API = 'http://localhost:8000/api'
+PREFIX_API = os.environ.get('DJANGO_PREFIX_API')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
