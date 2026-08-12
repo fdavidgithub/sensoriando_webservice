20320bfdc866f5f69947088ae28c2cca43ad5ebf
francis DAVID <contato@fdavid.com.br>
2026-08-12 20:17:59 -0300

    chore: esqueleto do SPA React com Vite e TypeScript

---
+++ web/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("root element is missing from index.html");
+}
+
+createRoot(container).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
